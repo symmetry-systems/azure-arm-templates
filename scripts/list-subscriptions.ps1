35b302ed0ebf725dc26279fc39e06d688dbf6189
Valter Simes <valter.simes@symmetry-systems.com>
2023-02-13 18:57:10 +0100

    Moved scripts

--- scripts/list-subscriptions.ps1
+++
@@ -1,6 +0,0 @@
-$subscriptions = @()
-Connect-AzAccount
-foreach ($sub in (Get-AzSubscription).Id) {
-        $subscriptions += '"'+$sub+'"'
-}
-$subscriptions -join ',' | Out-File "~/subscription-ids.txt"
